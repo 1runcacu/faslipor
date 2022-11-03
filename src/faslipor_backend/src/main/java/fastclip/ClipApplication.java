38ca4adc0a2da90300b78c7a2e529e6710874fc2
XQSUNgithub <2213770552@qq.com>
2022-11-03 22:56:22 +0800

    socketio-netty0.0

--- src/faslipor_backend/src/main/java/fastclip/ClipApplication.java
+++ src/faslipor_backend/src/main/java/fastclip/ClipApplication.java
@@ -13,55 +13,51 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.alibaba.fastjson.JSON;
 
 @SpringBootApplication
 @Slf4j
 public class ClipApplication {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(ClipApplication.class, args);
+        log.info("123");
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(8102);
+        SocketIOServer server = new SocketIOServer(config);
+        log.info("123");
+        server.addConnectListener(new ConnectListener() {// 添加客户端连接监听器
+            @Override
+            public void onConnect(SocketIOClient client) {
+                log.info(client.getRemoteAddress() + " web客户端接入");
+                HelloUid myData = new HelloUid();
+                myData.msg = "hello";
+                log.info(JSON.toJSONString(myData));
+                client.sendEvent("message", JSON.parseObject(JSON.toJSONString(myData)));
+            }
+        });
+        // 握手请求
 
-            Configuration config = new Configuration();
-            config.setHostname("localhost");
-            config.setPort(8102);
-            SocketIOServer server = new SocketIOServer(config);
-            server.addConnectListener(new ConnectListener() {// 添加客户端连接监听器
-                @Override
-                public void onConnect(SocketIOClient client) {
-                    log.info(client.getRemoteAddress() + " web客户端接入");
-                    client.sendEvent("helloPush", "hello");
-                }
-            });
-            // 握手请求
-            server.addEventListener("helloevent", HelloUid.class, new DataListener<HelloUid>() {
-                @Override
-                public void onData(final SocketIOClient client, HelloUid data, AckRequest ackRequest) {
-                    // 握手
-                    if (data.getMessage().equals("hello")) {
-                        int userid = data.getUid();
-                        log.info(Thread.currentThread().getName() + "web读取到的userid：" + userid);
+        server.addEventListener("message", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient client, String data, AckRequest ackRequest) throws Exception {
 
+                // 握手
+                log.info(data);
+                HelloUid myData = new HelloUid();
+                myData.msg = data;
+                // send message back to client with ack callback
+                // WITH data
+                client.sendEvent("message", JSON.parseObject(JSON.toJSONString(myData)));
 
-                        // send message back to client with ack callback
-                        // WITH data
-                        client.sendEvent("hellopush", new AckCallback<String>(String.class) {
-                            @Override
-                            public void onSuccess(String result) {
-                                log.info("ack from client: " + client.getSessionId() + " data: " + result);
-                            }
-                        }, 1000);
+            }
 
+        });
 
-                    } else {
-                        log.info("行情接收到了不应该有的web客户端请求1111...");
-                    }
-                }
-            });
-
-            server.start();
-
-            Thread.sleep(Integer.MAX_VALUE);
-
-            server.stop();
-        }
-
+        server.start();
+        log.info("123");
+        Thread.sleep(Integer.MAX_VALUE);
+        server.stop();
     }
+}
+
