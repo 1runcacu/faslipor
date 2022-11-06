eb221d25b45ca4e0fd6f85589bc6a108b4aa6acf
XQSUNgithub <2213770552@qq.com>
2022-11-06 19:21:32 +0800

    exit接口基本完成

---
+++ src/faslipor_backend/src/main/java/fastclip/domain/Frame.java
@@ -0,0 +1,9 @@
+package fastclip.domain;
+
+public class Frame {
+    /*frame:{String gid,String type, style:{int left,int top,int radius}*/
+    public String gid;
+    public String type;
+    public Style style;
+
+}
