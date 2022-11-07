3dfbe1c8ff7790d8ebdc4ea1eff1f941b28d793b
XQSUNgithub <2213770552@qq.com>
2022-11-07 16:53:20 +0800

    stream的all和increment

---
+++ src/faslipor_backend/src/main/java/fastclip/domain/Istream.java
@@ -0,0 +1,11 @@
+package fastclip.domain;
+
+import org.json.JSONObject;
+
+public class Istream {
+
+    public String event;
+    public String uid;
+    public String rid;
+    public Frame frame;
+}
