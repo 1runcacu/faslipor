8e4f30e601dea0e0d7ce46b2843c17e76cdd76f9
XQSUNgithub <2213770552@qq.com>
2022-11-07 15:24:30 +0800

    stream0.1

--- src/faslipor_backend/src/main/java/fastclip/domain/Stream.java
+++ src/faslipor_backend/src/main/java/fastclip/domain/Stream.java
@@ -3,7 +3,6 @@ package fastclip.domain;
 public class Stream {
     /*
     String event,String uid,String rid,frame:{String gid,String type, style:{int left,int top,int radius}}*/
-
     public String event;
     public String uid;
     public String rid;
