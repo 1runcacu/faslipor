5210e5f4f10729dd1ad4da5a382b5267d2963e7f
paiflo <1370523862@qq.com>
2025-07-06 18:47:50 +0800

    fix: 修bug

--- src/faslipor-front/src/api/config.js
+++ src/faslipor-front/src/api/config.js
@@ -6,8 +6,8 @@ export const APIURL = 'http://localhost:8101';
 // export const SOCKET = 'http://127.0.0.1:8099';
 // export const SOCKET = 'http://127.0.0.1:8400';
 
-// export const SOCKET = '/';
-export const SOCKET = 'http://120.24.215.12:8099';
+export const SOCKET = '/';
+// export const SOCKET = 'http://120.24.215.12:8099';
 
 export default{
     APIURL,
